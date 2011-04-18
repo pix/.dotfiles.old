ef4bc048d23827198ab2bc178a55b1694ceb6867
Camille Moncelier <moncelier@devlife.org>
2011-04-18 15:15:34 +0200

    [zsh] Added arrow prompt theme

---
+++ zsh/zsh/rc/theme-and-appearance.zsh
@@ -0,0 +1,13 @@
+# ls colors
+autoload colors; colors;
+
+setopt auto_cd
+setopt multios
+setopt cdablevarS
+setopt prompt_subst
+
+# git theming default: Variables for theming the git info prompt
+ZSH_THEME_GIT_PROMPT_PREFIX="git:("         # Prefix at the very beginning of the prompt, before the branch name
+ZSH_THEME_GIT_PROMPT_SUFFIX=")"             # At the very end of the prompt
+ZSH_THEME_GIT_PROMPT_DIRTY="*"              # Text to display if the branch is dirty
+ZSH_THEME_GIT_PROMPT_CLEAN=""               # Text to display if the branch is clean
